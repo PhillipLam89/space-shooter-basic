4ab5a4755b508f335ae019f3fd0c8bd1a5d97f06
Phillip Lam <plam89@ucla.edu>
2023-10-08 17:43:00 -0700

    finally able to render player airplane on screen

--- index.js
+++ index.js
@@ -9,15 +9,27 @@ class Player {
   constructor() {
     this.position = {x:200, y:200}
     this.velocity = {x: 0, y:0}
-    //this.image =
+
+    const image = new Image()
+    image.src = './resources/spaceship.png'
+    this.image = image
+
     this.width = 100
     this.height= 100
   }
   draw() {
-    c.fillStyle = 'red'
-    c.fillRect(this.position.x, this.position.y,
-              this.width, this.height)
+    // c.fillStyle = 'red'
+    // c.fillRect(this.position.x, this.position.y,
+    //           this.width, this.height)
+    c.drawImage(this.image, this.position.x,
+      this.position.y)
   }
 }
 const player = new Player()
 player.draw()
+
+function animate() {
+  requestAnimationFrame(animate)
+  player.draw()
+}
+animate()
