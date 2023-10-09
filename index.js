17d9abffca1e882e057f1c927f24a16b978125ec
Phillip Lam <plam89@ucla.edu>
2023-10-08 18:00:00 -0700

    final touches on player positon etc

--- index.js
+++ index.js
@@ -7,22 +7,29 @@ canvas.height = window.innerHeight
 
 class Player {
   constructor() {
-    this.position = {x:200, y:200}
+
     this.velocity = {x: 0, y:0}
 
     const image = new Image()
     image.src = './resources/spaceship.png'
-    this.image = image
-
-    this.width = 100
-    this.height= 100
+    image.onload = () => {
+      this.image = image
+      const scale = .15
+    this.width = image.width * scale
+    this.height= image.height * scale
+    this.position = {
+      x: canvas.width * .5 - this.width *.5,
+      y: canvas.height - this.height - 20
+    }
+    }
   }
   draw() {
     // c.fillStyle = 'red'
     // c.fillRect(this.position.x, this.position.y,
     //           this.width, this.height)
+    this.image &&
     c.drawImage(this.image, this.position.x,
-      this.position.y)
+      this.position.y,this.width, this.height)
   }
 }
 const player = new Player()
@@ -30,6 +37,8 @@ player.draw()
 
 function animate() {
   requestAnimationFrame(animate)
+  c.fillStyle = 'goldenrod'
+  c.fillRect(0,0, canvas.width, canvas.height)
   player.draw()
 }
 animate()
